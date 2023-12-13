b6344a56827891d08d2d92f46cee5149ae55abb1
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2023-12-13 14:18:59 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=""
+noseY=""
 function preload() {
-    
+    clown_nose=loadImage("https://i.postimg.cc/N0Zdq2hZ/Moustache-PNG-Pic.png")
 }
 function setup() {
     canvas=createCanvas(300,300)
@@ -11,7 +13,14 @@ function setup() {
     postnet.on('pose',got_poses)
 }
 function draw() {
-    image(video,0,0,300,300)}
+    image(video,0,0,300,300)
+    fill("red")
+    stroke("red")
+//circle(noseX,noseY,20)
+image(clown_nose,noseX - 35,noseY -10,75,50)
+
+
+}
 function take_snapshot() {
     save('my_selfie.png')
 }
@@ -21,7 +30,10 @@ function model_loaded() {
 function got_poses(results) {
     if (results.length>0) {
         console.log(results)
-        console.log("mouth x=" + results[0].pose.nose.x)
-        console.log("mouth y=" + results[0].pose.nose.y)
+        console.log("nose x=" + results[0].pose.nose.x)
+        console.log("nose y=" + results[0].pose.nose.y)
+        noseX=results[0].pose.nose.x
+        noseY=results[0].pose.nose.y
+
     }
 }
